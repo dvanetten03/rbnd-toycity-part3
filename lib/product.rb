b7e56c0972f4bc045a102423376b68d94ac80a81
Diane Van Etten <diane.vanetten@gmail.com>
2016-08-20 22:52:45 -0500

    Add duplicate error

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,15 @@
 class Product
-  # attr_reader :title, :price, :brand
+  attr_reader :title
   # attr_accessor :stock
 
   @@products = []
 
   def initialize(options={})
-    # @title = options[:title]
+    @title = options[:title]
     # @price = options[:price]
     # @brand = options[:brand]
     # @stock = options[:stock]
-    @@products << self
+    add_to_products
   end
 
   def self.all
@@ -18,11 +18,11 @@ class Product
 
   # private
 
-  # def add_to_products
-  #   if @@products.map { |product| product.title}.include? @title
-  #   @@products<<self
-  #   else
-  #     raise DuplicateProductError
-  #   end
-  # end
+  def add_to_products
+    if @@products.map { |product| product.title}.include? @title
+    @@products<<self
+    else
+      raise DuplicateProductError
+    end
+  end
 end
