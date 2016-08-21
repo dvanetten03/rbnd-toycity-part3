f4c455fe215961e3980441bd42a386c159452b36
Diane Van Etten <diane.vanetten@gmail.com>
2016-08-20 21:08:36 -0500

    Add ability to create and count products.

---
+++ lib/customer.rb
@@ -0,0 +1,13 @@
+
+# class Customer
+
+#   @@customers = []
+
+#   attr_reader :name
+
+#   def initialize(options={})
+#     @name = options[:name]
+#     add_to_customers
+#   end
+
+# end
--- lib/product.rb
+++ lib/product.rb
@@ -1,28 +1,28 @@
 class Product
-  attr_reader :title, :price, :brand
-  attr_accessor :stock
+  # attr_reader :title, :price, :brand
+  # attr_accessor :stock
 
   @@products = []
 
   def initialize(options={})
-    @title = options[:title]
-    @price = options[:price]
-    @brand = options[:brand]
-    @stock = options[:stock]
-    add_to_products
+    # @title = options[:title]
+    # @price = options[:price]
+    # @brand = options[:brand]
+    # @stock = options[:stock]
+    @@products << self
   end
 
   def self.all
     @@products
   end
 
-  private
+  # private
 
-  def add_to_products
-    if @@products.map { |product| product.title}.include? @title
-    @@products<<self
-    else
-      raise DuplicateProductError
-    end
-  end
+  # def add_to_products
+  #   if @@products.map { |product| product.title}.include? @title
+  #   @@products<<self
+  #   else
+  #     raise DuplicateProductError
+  #   end
+  # end
 end
